041943c4b005766dd92f15f69cd8f33ed244b9e0
sooparkdev <135008690+sooparkdev@users.noreply.github.com>
2020-05-26 21:15:09 -0700

    add pie chart

--- Midpoint Deliverable/scripts/piechart.R
+++ Midpoint Deliverable/scripts/piechart.R
@@ -1,23 +1,25 @@
 library("dplyr")
 library("ggplot2")
 
-# creates pie chart using the COVID.19.Deaths column in the dataframe passed in
-# adds up the COVID 19 deaths by states and organize them in a descending order
 create_pie_chart <- function(dataframe) {
   filtered_df <- health_data %>%
     group_by(State) %>%
+    # filters redundant information
     filter(Age.group == "All Ages") %>%
     filter(State != "United States") %>%
     filter(State != "United States Total") %>%
     summarize(
       covid_death = sum(COVID.19.Deaths, na.rm = TRUE)
     ) %>%
+    # arranges the dataframe in a descending order
     arrange(-covid_death) %>%
+    # leaves only ten rows with the highest covid deaths
     head(10)
   pie_chart <- ggplot(data = filtered_df,
               aes(x = "", y = covid_death, color = State, fill = State)) +
     geom_bar(stat = "identity", width = 1, color = "white") +
     coord_polar("y", start = 0) +
+    # gets rid of unnecessary factors in the plot
     ggtitle("Top 10 States with Highest COVID19 Deaths") +
     theme(panel.background = element_blank(),
           axis.line = element_blank(),
