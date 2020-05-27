0d6f9993e55841a48cdc9b805fbdfabc80abe1ba
sooparkdev <135008690+sooparkdev@users.noreply.github.com>
2020-05-27 11:57:56 +0800

    Make changes

--- Midpoint Deliverable/scripts/tableFunction.R
+++ Midpoint Deliverable/scripts/tableFunction.R
@@ -5,21 +5,26 @@ health_data <- read.csv("data/us-deaths.csv"
 
 get_table <- function(data) {
   dataset <- data %>%
+    # makes new columns for %
     mutate(Covid_percent = round((COVID.19.Deaths /
                                     Total.Deaths) * 100, digits = 2),
            Pneunomia_percent = round((Pneumonia.Deaths /
                                         Total.Deaths) * 100, digits = 2),
            Influenza_percent = round((Influenza.Deaths /
                                         Total.Deaths) * 100, digits = 2)) %>%
+    # leaves only these four columns
     select(State, Covid_percent,
            Pneunomia_percent, Influenza_percent) %>%
     group_by(State) %>%
+    # leaves only the highest % of each state
     summarise(total_Covid_death_percentage = max(Covid_percent),
               total_Pneumonia_death_percentage = max(Pneunomia_percent),
               total_Influenza_death_percentage = max(Influenza_percent)) %>%
+    # gets rid of na rows
     filter(!is.na(total_Covid_death_percentage),
            !is.na(total_Influenza_death_percentage),
            !is.na(total_Pneumonia_death_percentage)) %>%
+    # highest to lowest covid % and only top 10 values
     arrange(-total_Covid_death_percentage) %>%
     head(10)
   return(dataset)
